814055e7d98c3de1ff2ce9a9ba07715561308ca9
Administrator <admin@example.com>
2019-02-13 18:49:48 +0530

    Fixed java container issue

--- src/main/java/org/otp/orgtoolssuite/OrgToolsSuiteApplication.java
+++ src/main/java/org/otp/orgtoolssuite/OrgToolsSuiteApplication.java
@@ -1,8 +1,11 @@
 package org.otp.orgtoolssuite;
 
+import org.otp.orgtoolssuite.controllers.TestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("org.otp.orgtoolssuite")
 @SpringBootApplication
 public class OrgToolsSuiteApplication {
 
